811aa9921d5dfd59ea3718494ed4ec5a26e294b1
Sejal <73702888+octajune@users.noreply.github.com>
2023-07-01 15:46:37 -0400

    Applying the CSS changes to the landing page

--- src/components/home/Landing.jsx
+++ src/components/home/Landing.jsx
@@ -1,6 +1,19 @@
 import React from "react";
 import "./Landing.css";
+import { ThemeProvider } from 'styled-components';
 import ChatBot from 'react-simple-chatbot';
+
+const otherFontTheme = {
+  background: '#f5f8fb',
+  fontFamily: 'Helvetica Neue',
+  headerBgColor: '#6e48aa',
+  headerFontColor: '#fff',
+  headerFontSize: '16px',
+  botBubbleColor: '#6E48AA',
+  botFontColor: '#fff',
+  userBubbleColor: '#fff',
+  userFontColor: '#4a4a4a'
+};
 
 function Landing({ handleClick }) {
   return (
@@ -14,33 +27,37 @@ function Landing({ handleClick }) {
       </button>
       <img src="/Mentee.png" alt="mentee"id="mentee-image" />
       </div>
-      <ChatBot
-        steps={[
-          {
-            id: '1',
-            message: 'What number I am thinking?',
-            trigger: '2',
-          },
-          {
-            id: '2',
-            options: [
-              { value: 1, label: 'Number 1', trigger: '4' },
-              { value: 2, label: 'Number 2', trigger: '3' },
-              { value: 3, label: 'Number 3', trigger: '3' },
-            ],
-          },
-          {
-            id: '3',
-            message: 'Wrong answer, try again.',
-            trigger: '2',
-          },
-          {
-            id: '4',
-            message: 'Awesome! You are a telepath!',
-            end: true,
-          },
-        ]}
-      />
+      <ThemeProvider theme={otherFontTheme}>
+      <div className="landing-chatbot">
+        <ChatBot
+          steps={[
+            {
+              id: '1',
+              message: 'What number I am thinking?',
+              trigger: '2',
+            },
+            {
+              id: '2',
+              options: [
+                { value: 1, label: 'Number 1', trigger: '4' },
+                { value: 2, label: 'Number 2', trigger: '3' },
+                { value: 3, label: 'Number 3', trigger: '3' },
+              ],
+            },
+            {
+              id: '3',
+              message: 'Wrong answer, try again.',
+              trigger: '2',
+            },
+            {
+              id: '4',
+              message: 'Awesome! You are a telepath!',
+              end: true,
+            },
+          ]}
+        />
+      </div>
+      </ThemeProvider>
     </div>
     
   );
