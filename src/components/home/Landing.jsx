85d88db0f714ceeb09d85c3067910eebb299de12
Sejal <73702888+octajune@users.noreply.github.com>
2023-07-01 15:28:02 -0400

    Adding chatbot integration

--- src/components/home/Landing.jsx
+++ src/components/home/Landing.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Landing.css";
+import ChatBot from 'react-simple-chatbot';
 
 function Landing({ handleClick }) {
   return (
@@ -13,7 +14,35 @@ function Landing({ handleClick }) {
       </button>
       <img src="/Mentee.png" alt="mentee"id="mentee-image" />
       </div>
+      <ChatBot
+        steps={[
+          {
+            id: '1',
+            message: 'What number I am thinking?',
+            trigger: '2',
+          },
+          {
+            id: '2',
+            options: [
+              { value: 1, label: 'Number 1', trigger: '4' },
+              { value: 2, label: 'Number 2', trigger: '3' },
+              { value: 3, label: 'Number 3', trigger: '3' },
+            ],
+          },
+          {
+            id: '3',
+            message: 'Wrong answer, try again.',
+            trigger: '2',
+          },
+          {
+            id: '4',
+            message: 'Awesome! You are a telepath!',
+            end: true,
+          },
+        ]}
+      />
     </div>
+    
   );
 }
 
